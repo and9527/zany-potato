5a311fbc50707b3fd2aa24709c5b7ee237403937
and9527 <and95277@gmail.com>
2023-03-02 01:54:07 +0000

    Pending changes exported from your codespace

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import ClickCount from '../components/ClickCount'
 import styles from '../styles/home.module.css'
 
 function throwError() {
+  console.log('asdasd');
   console.log(
     // The function body() is not defined
     document.body()
